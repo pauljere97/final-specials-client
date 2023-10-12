faf4f223d88b49538fa5376543218fc2f5da1d7e
Paul Jere <pauljere97@gmail.com>
2023-10-12 23:24:11 +0200

    to developemnt env

--- src/app/utils/urls.ts
+++ src/app/utils/urls.ts
@@ -1,9 +1,9 @@
 const development = {
-    baseUrl: "http://51.20.31.71:8080/"
+    baseUrl: "http://127.0.0.1:8080/"
 };
 const production = {
     baseUrl: "http://16.171.199.141:8080/"
 };
 
-// export const urls = development;
-export const urls = production;
+export const urls = development;
+// export const urls = production;
