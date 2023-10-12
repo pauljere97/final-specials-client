986459ad5a55b5eec60be24be759b989892d486c
Paul Jere <pauljere97@gmail.com>
2023-10-12 21:32:45 +0200

    use hash

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,6 +2,8 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LoginComponent } from './externals/login/login.component';
 import { MatchInputComponent } from './internals/match-input/match-input.component';
+import { CommonModule } from '@angular/common';
+import { BrowserModule } from '@angular/platform-browser';
 
 const routes: Routes = [
   {
@@ -20,7 +22,13 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  imports: [
+    CommonModule,
+    BrowserModule,
+    RouterModule.forRoot(routes,{
+       useHash: true
+    })
+  ],
+  exports: [RouterModule],
 })
 export class AppRoutingModule { }
